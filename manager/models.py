ad3e4f65c79a19237f6468b2cc12f2d06534793e
Tobias Rosskopf (privat) <tobirosskopf@gmail.com>
2019-12-15 00:10:05 +0100

    Added modell for songs

--- manager/models.py
+++ manager/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+from datetime import timedelta
+
+class Song(models.Model):
+    name = models.CharField(max_length=200)
+    interpret = models.CharField(max_length=200)
+    # duration = models.DurationField(default=timedelta(0))
+
+    active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"{self.name} ({self.interpret})"
--- manager/views.py
+++ manager/views.py
@@ -1,3 +1,14 @@
+from django.http import HttpResponse
+from django.template import loader
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Song
+
+
+def songs(request):
+    song_list = Song.objects.order_by('name')
+    template = loader.get_template('manager/songs.html')
+    context = {
+        'song_list': song_list,
+    }
+    return HttpResponse(template.render(context, request))
